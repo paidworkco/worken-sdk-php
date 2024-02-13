74ff7cddccd2a8877cabd6c51bc9f7c0819ad825
xdbx114 <Arturk8791@hotmail.com>
2024-02-13 17:50:47 +0100

    Fix getEstimatedGas

--- src/services/NetworkService.php
+++ src/services/NetworkService.php
@@ -2,9 +2,6 @@
 namespace Worken\Services;
 
 use Web3\Web3;
-use Web3\Contract;
-use Web3\Utils;
-use kornrunner\Keccak;
 
 class NetworkService {
     private $web3;
@@ -31,13 +28,11 @@ class NetworkService {
     }
 
     // testing
-    public function getEstimatedGas($from, $to, int $amount) {
-        $data = $this->encodeTransferData($to, $amount);
-    
+    public function getEstimatedGas($from, $to, $amount) {
         $transaction = [
             'from' => $from,
-            'to' => $this->contractAddress,
-            'data' => $data
+            'to' => $to,
+            'amount' => $amount
         ];
     
         $this->web3->eth->estimateGas($transaction, function ($err, $gas) {
@@ -55,56 +50,5 @@ class NetworkService {
 
     public function getMonitorCongestion() {
 
-    }
-
-    //testing
-    private function getERC20ABI() {
-        return '[
-            {
-                "constant": true,
-                "inputs": [{"name": "_owner", "type": "address"}],
-                "name": "balanceOf",
-                "outputs": [{"name": "balance", "type": "uint256"}],
-                "type": "function"
-            },
-            {
-                "constant": false,
-                "inputs": [
-                    {"name": "_to", "type": "address"},
-                    {"name": "_value", "type": "uint256"}
-                ],
-                "name": "transfer",
-                "outputs": [{"name": "", "type": "bool"}],
-                "type": "function"
-            },
-            {
-                "constant": false,
-                "inputs": [
-                    {"name": "_spender", "type": "address"},
-                    {"name": "_value", "type": "uint256"}
-                ],
-                "name": "approve",
-                "outputs": [{"name": "", "type": "bool"}],
-                "type": "function"
-            },
-            {
-                "constant": true,
-                "inputs": [
-                    {"name": "_owner", "type": "address"},
-                    {"name": "_spender", "type": "address"}
-                ],
-                "name": "allowance",
-                "outputs": [{"name": "remaining", "type": "uint256"}],
-                "type": "function"
-            }
-        ]';
-    }
-
-    //testing
-    function encodeTransferData($to, $amount) {
-        $selector = substr(Keccak::hash('transfer(address,uint256)', 256), 0, 8);
-        $addressPadded = str_pad(substr($to, 2), 64, '0', STR_PAD_LEFT);
-        $amountPadded = str_pad(dechex($amount), 64, '0', STR_PAD_LEFT);
-        return '0x' . $selector . $addressPadded . $amountPadded;
     }
 }
